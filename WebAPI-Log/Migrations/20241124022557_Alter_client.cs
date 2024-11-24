7d8f93812a76b528c5f16bc9e14c4fc789717d38
balta <baltaasanchez@gmail.com>
2024-11-23 23:26:13 -0300

    Add-migration

---
+++ WebAPI-Log/Migrations/20241124022557_Alter_client.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI_Log.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alter_client : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ContactNro",
+                table: "Cliente",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactNro",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Cliente");
+        }
+    }
+}
